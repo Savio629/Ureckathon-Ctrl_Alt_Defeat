9a0665004695db63c7a04c74bc9008ce6f8dc690
Savio629 <diassavio629@gmail.com>
2023-02-26 13:54:36 +0530

    Added authentication

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,9 +3,9 @@
 import React from "react";
 import "./styles1.css";
 import { Link } from "react-router-dom";
-
+import {useAuth0} from '@auth0/auth0-react';
 export default function Nav(){
-
+  const {user, loginWithRedirect , isAuthenticated, logout} = useAuth0();
   return(
         <div className="navbar">
           <div className="logo">MindShine</div>
@@ -16,7 +16,17 @@ export default function Nav(){
               <Link to="/desktop-1">Assessment</Link>
               <Link to="/analytics">Analytics</Link>
               <Link to="/community">Community</Link>
-              <Link to="/blog">Blog</Link>            
+              <Link to="/blog">Blog</Link>
+              {isAuthenticated &&
+               ( <p className="emailid"> {user.name} </p>)}
+              {
+                isAuthenticated ? (
+                <button className="loginb"
+                onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>Log Out
+                </button>  
+                ) : (  
+                <button className="loginb" onClick={()=> loginWithRedirect()}>Log In</button>     
+               )}            
            </ul>
         </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,13 +3,22 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "./global.css";
+import {Auth0Provider} from '@auth0/auth0-react';
 // import cursor1 from "./styles/cursor.png"
 // import { emojiCursor } from "cursor-effects";
 // new emojiCursor({ emoji: ["🐱", " "] });
 
 ReactDOM.render(
   <React.StrictMode>
+ <Auth0Provider
+  domain="dev-blapvmv4askhods2.us.auth0.com"
+  clientId="SGGGXXzgXFOqCYTdsXlrJa3ZaE7JKncS"
+  authorizationParams={{
+    redirect_uri: window.location.origin,
+  }}
+> 
     <App />
+  </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
