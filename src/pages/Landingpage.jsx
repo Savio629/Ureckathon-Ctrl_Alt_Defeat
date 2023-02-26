301ace6b59e313c92a0b17f9852bd3ec92b8485c
Savio629 <diassavio629@gmail.com>
2023-02-26 18:44:59 +0530

    Enhance the homepage

--- src/pages/Landingpage.jsx
+++ src/pages/Landingpage.jsx
@@ -1,48 +1,76 @@
-import React from 'react';
+
 //  import './Landingpage.css';
+import React, { useState } from "react";
+import img1 from "./img1.png";
+import img2 from "./img2.png";
+import imgm1 from "./imgm1.png"
 import './lpage.css';
 //import homepageimg from './assets/homepageimg.png';
-
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Landingpage = () => {
-  return (
+    const [showImg2, setShowImg2] = useState(false);
+
+    const toggleImg = () => {
+      setShowImg2((prevState) => !prevState);
+    };
+    return (
     <>
         <main>
-        <div class="big-wrapper">
-            <img src="./img/shape.png" class="shape" alt=""/>
-            <header>
-                <div class="contain">
-                    <div >
-                        MINDSHINE
-                    </div>
-                </div>
-            </header>
+        <h1 className="logotext">MINDSHINE</h1>
+         <img className="imgm"src={imgm1}/>
 
-            <div class="showcase-area">
-                <div class="container">
-                <div class="left">
-                    <div class="big-title">
-                        <h2 id="start-playing-text">Mind Matters!</h2>
-                        <h2 id="start-playing-text">Start Healing!<br></br></h2>
-                        
-                    </div>
-                    <p class="text">You're not alone. MindShine is here to provide you with the support and guidance you need to get back on track. Our interactive tools and resources are here to help you find the right path. Take the first step towards a healthier, happier life today with MindShine.
-                    </p>
-                </div>
-                <div class="right">
-                    <img src="./assets/homepageimg1.png" alt=" Image" class="console"/>
-                </div>
-            </div>              
-            </div>
+        <h2 className="mainh2">Find hope, healing, and strength in our community dedicated to improving mental wellness</h2>
+        {/* <img src={gifg} alt="loading..." /> */}
+              
+        <section><div className="imgBx">
+            
+          
+         
 
-            <div class="bottom-area">
-                <div class="container">
-                  
-                </div>
-            </div>
+          <img src={img1} />
         </div>
+        <div className="content">
+          <h2></h2>
+          <p>
+          Mental health is about being emotionally and socially healthy - the way we think, feel and develop relationships - and not merely the absence of a mental health condition.Mental stress, also known as psychological stress, is a type of stress that affects a person's mental and emotional well-being. It can be caused by a wide variety of factors, including work-related pressures, financial difficulties, family problems, relationship issues, and health concerns, among others.
+          </p>
+        </div>
+      </section>
+      <section>
+        <div className="imgBx">
+          <img src={img2} />
+        </div>
+        <div className="content">
+          <h2></h2>
+          <p>
+          It is also important to address any underlying issues that may be contributing to mental health challenges. This may include addressing past trauma, improving communication skills, or developing better coping mechanisms.
+Ultimately, the key to curing mental health is to approach it holistically and to develop a personalized treatment plan that takes into account each individual's unique needs and circumstances. With the right support, resources, and strategies, it is possible to manage mental health challenges and live a fulfilling and meaningful life.
+          </p>
+        </div>
+      </section>
+      <section>
+        <div className="imgBx">
+          {/* <img
+            src={require("./img1.png")}
+            onMouseOver={(this.src = require("./img2.png"))}
+            onMouseOut={(this.src = require("./img1.png"))}
+          /> */}
+          <img
+            src={showImg2 ? img2 : img1}
+            onMouseEnter={toggleImg}
+            onMouseLeave={toggleImg}
+            alt="Image"
+          />
+        </div>
+        <div className="content">
+          <h2>Text for Div 3</h2>
+          <p>
+          Mental stress is also often viewed as a response to external circumstances or events that cause us to feel anxious, overwhelmed, or worried. Mental stress is not just the result of external factors, but is also shaped by our internal responses and interpretations. By learning to recognize and manage our thoughts and beliefs, we can reduce the impact of mental stress on our well-being and cultivate greater happiness and resilience in our lives.
+          </p>
+        </div>
+      </section>
     </main>
-
     </>
   )
 }
